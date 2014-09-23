351c02063ddf0e26e23d3c17e69d2794de90b178
Jan Marthedal Rasmussen <me@janmr.com>
2014-09-23 15:05:57 +0200

    highlight/demo2

---
+++ highlight/demo2.js
@@ -0,0 +1,144 @@
+﻿var UniMath2;
+(function (UniMath2) {
+    'use strict';
+
+    function elementWithClass(tagName, className) {
+        var el = document.createElement(tagName);
+        el.className = className;
+        return el;
+    }
+
+    function addClass(el, cls) {
+        var cur = el.className.trim();
+        if (!cur) {
+            el.className = cls;
+        } else if ((' ' + cur + ' ').indexOf(' ' + cls + ' ') < 0) {
+            el.className = cur + ' ' + cls;
+        }
+    }
+
+    function removeClass(el, cls) {
+        el.className = (' ' + el.className + ' ').replace(' ' + cls + ' ', ' ').trim();
+    }
+
+    var curFocusElement = null;
+    var curHoverElement = null;
+
+    function updateFocus(newFocusElement, newHoverElement) {
+        var hasFocusClass = curFocusElement !== null ? curFocusElement : curHoverElement;
+        var hasHoverClass = curFocusElement !== null && curFocusElement !== curHoverElement ? curHoverElement : null;
+        var getFocusClass = newFocusElement !== null ? newFocusElement : newHoverElement;
+        var getHoverClass = newFocusElement !== null && newFocusElement !== newHoverElement ? newHoverElement : null;
+        if (hasFocusClass !== getFocusClass) {
+            if (hasFocusClass !== null) {
+                removeClass(hasFocusClass, 'focus');
+                hasFocusClass.blur();
+            }
+            if (getFocusClass !== null) {
+                addClass(getFocusClass, 'focus');
+                getFocusClass.focus();
+            }
+        }
+        if (hasHoverClass !== getHoverClass) {
+            if (hasHoverClass !== null)
+                removeClass(hasHoverClass, 'hover');
+            if (getHoverClass !== null)
+                addClass(getHoverClass, 'hover');
+        }
+        curFocusElement = newFocusElement;
+        curHoverElement = newHoverElement;
+    }
+
+    function zoomAction() {
+        alert('zoom');
+    }
+    function menuAction() {
+        alert('menu');
+    }
+
+    function init() {
+        var nodelist = document.getElementsByClassName('unimath');
+        Array.prototype.forEach.call(nodelist, function (el, index) {
+            var activeAction = 0;
+            var zoomEl = elementWithClass('span', 'zoom fa fa-search-plus on');
+            var menuEl = elementWithClass('span', 'menu fa fa-external-link');
+
+            function currentActionElement() {
+                return activeAction === 0 ? zoomEl : menuEl;
+            }
+
+            function changeActiveAction(newAction) {
+                if (newAction !== activeAction) {
+                    removeClass(currentActionElement(), 'on');
+                    activeAction = newAction;
+                    addClass(currentActionElement(), 'on');
+                }
+            }
+
+            function triggerActiveAction() {
+                updateFocus(null, null);
+                if (activeAction === 0) {
+                    zoomAction();
+                } else {
+                    menuAction();
+                }
+            }
+
+            el.setAttribute('tabindex', '0');
+
+            el.addEventListener('focus', function () {
+                updateFocus(el, curHoverElement);
+            }, true);
+            el.addEventListener('blur', function () {
+                updateFocus(null, curHoverElement);
+            }, true);
+
+            el.addEventListener('mouseenter', function () {
+                updateFocus(curFocusElement, el);
+            }, false);
+            el.addEventListener('mouseleave', function () {
+                updateFocus(curFocusElement, null);
+            }, false);
+
+            el.addEventListener('keydown', function (ev) {
+                switch (ev.keyCode) {
+                    case 37:
+                    case 39:
+                        changeActiveAction((activeAction + 1) % 2);
+                        break;
+                    case 90:
+                        activeAction = 0;
+                        triggerActiveAction();
+                        break;
+                    case 77:
+                        activeAction = 1;
+                        triggerActiveAction();
+                        break;
+                    case 13:
+                        triggerActiveAction();
+                        break;
+                }
+            });
+
+            menuEl.addEventListener('mouseenter', function () {
+                changeActiveAction(1);
+            }, true);
+            menuEl.addEventListener('mouseleave', function () {
+                changeActiveAction(0);
+            }, true);
+
+            zoomEl.addEventListener('click', zoomAction, false);
+            menuEl.addEventListener('click', menuAction, false);
+            el.addEventListener('click', triggerActiveAction, false);
+
+            var actionsEl = elementWithClass('div', 'actions');
+            actionsEl.appendChild(zoomEl);
+            actionsEl.appendChild(menuEl);
+            el.appendChild(actionsEl);
+        });
+    }
+    UniMath2.init = init;
+})(UniMath2 || (UniMath2 = {}));
+
+UniMath2.init();
+//# sourceMappingURL=demo2.js.map
---
+++ highlight/demo2.ts
@@ -0,0 +1,130 @@
+﻿module UniMath2 {
+    'use strict';
+
+    function elementWithClass(tagName: string, className: string): HTMLElement {
+        var el: HTMLElement = document.createElement(tagName);
+        el.className = className;
+        return el;
+    }
+
+    function addClass(el: HTMLElement, cls: string): void {
+        var cur: string = el.className.trim();
+        if (!cur) {
+            el.className = cls;
+        } else if ((' ' + cur + ' ').indexOf(' ' + cls + ' ') < 0) {
+            el.className = cur + ' ' + cls;
+        }
+    }
+
+    function removeClass(el: HTMLElement, cls: string): void {
+        el.className = (' ' + el.className + ' ').replace(' ' + cls + ' ', ' ').trim();
+    }
+
+    var curFocusElement: HTMLElement = null;
+    var curHoverElement: HTMLElement = null;
+
+    function updateFocus(newFocusElement: HTMLElement, newHoverElement: HTMLElement): void {
+        var hasFocusClass: HTMLElement = curFocusElement !== null ? curFocusElement : curHoverElement;
+        var hasHoverClass: HTMLElement = curFocusElement !== null && curFocusElement !== curHoverElement ? curHoverElement : null;
+        var getFocusClass: HTMLElement = newFocusElement !== null ? newFocusElement : newHoverElement;
+        var getHoverClass: HTMLElement = newFocusElement !== null && newFocusElement !== newHoverElement ? newHoverElement : null;
+        if (hasFocusClass !== getFocusClass) {
+            if (hasFocusClass !== null) {
+                removeClass(hasFocusClass, 'focus');
+                hasFocusClass.blur();
+            }
+            if (getFocusClass !== null) {
+                addClass(getFocusClass, 'focus');
+                getFocusClass.focus();
+            }
+        }
+        if (hasHoverClass !== getHoverClass) {
+            if (hasHoverClass !== null)
+                removeClass(hasHoverClass, 'hover');
+            if (getHoverClass !== null)
+                addClass(getHoverClass, 'hover');
+        }
+        curFocusElement = newFocusElement;
+        curHoverElement = newHoverElement;
+    }
+
+    function zoomAction(): void {
+        alert('zoom');
+    }
+    function menuAction(): void {
+        alert('menu');
+    }
+
+    export function init(): void {
+        var nodelist: NodeList = document.getElementsByClassName('unimath');
+        Array.prototype.forEach.call(nodelist, (el: HTMLElement, index: number): void => {
+            var activeAction: number = 0;
+            var zoomEl: HTMLElement = elementWithClass('span', 'zoom fa fa-search-plus on');
+            var menuEl: HTMLElement = elementWithClass('span', 'menu fa fa-external-link');
+
+            function currentActionElement(): HTMLElement {
+                return activeAction === 0 ? zoomEl : menuEl;
+            }
+
+            function changeActiveAction(newAction: number): void {
+                if (newAction !== activeAction) {
+                    removeClass(currentActionElement(), 'on');
+                    activeAction = newAction;
+                    addClass(currentActionElement(), 'on');
+                }
+            }
+
+            function triggerActiveAction(): void {
+                updateFocus(null, null);
+                if (activeAction === 0) {
+                    zoomAction();
+                } else {
+                    menuAction();
+                }
+            }
+
+            el.setAttribute('tabindex', '0');
+
+            el.addEventListener('focus', (): void => { updateFocus(el, curHoverElement); }, true);
+            el.addEventListener('blur', (): void => { updateFocus(null, curHoverElement); }, true);
+
+            el.addEventListener('mouseenter', (): void => { updateFocus(curFocusElement, el); }, false);
+            el.addEventListener('mouseleave', (): void => { updateFocus(curFocusElement, null); }, false);
+
+            el.addEventListener('keydown', (ev: KeyboardEvent): void => {
+                // console.log('keydown', ev.keyCode);
+                switch (ev.keyCode) {
+                    case 37: // left arrow
+                    case 39: // right arrow
+                        changeActiveAction((activeAction + 1) % 2);
+                        break;
+                    case 90: // 'z'
+                        activeAction = 0;
+                        triggerActiveAction();
+                        break;
+                    case 77: // 'm'
+                        activeAction = 1;
+                        triggerActiveAction();
+                        break;
+                    case 13: // return
+                        triggerActiveAction();
+                        break;
+                }
+            });
+
+            menuEl.addEventListener('mouseenter', (): void => { changeActiveAction(1); }, true);
+            menuEl.addEventListener('mouseleave', (): void => { changeActiveAction(0); }, true);
+
+            zoomEl.addEventListener('click', zoomAction, false);
+            menuEl.addEventListener('click', menuAction, false);
+            el.addEventListener('click', triggerActiveAction, false);
+
+            var actionsEl: HTMLElement = elementWithClass('div', 'actions');
+            actionsEl.appendChild(zoomEl);
+            actionsEl.appendChild(menuEl);
+            el.appendChild(actionsEl);
+        });
+    }
+}
+
+UniMath2.init();
