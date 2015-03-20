8f90108ae0be9b3f14857ad104b93c48c319f45b
Jan Marthedal Rasmussen <jan@janmr.com>
2015-03-20 14:20:12 +0100

    wolfram demo update

--- wolfram/app.js
+++ wolfram/app.js
@@ -2,6 +2,13 @@
 /// <reference path="jquery.d.ts" />
 var FlorianMath;
 (function (FlorianMath) {
+    function map(list, fn) {
+        var result = [];
+        FlorianMath.each(list, function (item) {
+            result.push(fn(item));
+        });
+        return result;
+    }
     var log = function (a1, a2) {
     };
     if ('console' in window)
@@ -11,8 +18,16 @@ var FlorianMath;
             else
                 console.log(a1);
         };
+    function getWidth(el) {
+        var b = el.getBoundingClientRect();
+        return b.right - b.left;
+    }
+    function stopEvent(ev) {
+        ev.stopPropagation();
+        ev.preventDefault();
+    }
     function onFocus() {
-        var contentElement, $item = $(this), menu = $('<div class="math-ui-focus-menu"><div class="eraser"/><span>Equation 2</span><span class="icon icon-menu" /><span class="icon icon-zoom-in" /><span class="icon icon-star-empty" /></div>');
+        var contentElement, $item = $(this), display_inline, eraser = $('<div class="eraser"/>'), name = $('<span/>').append('Equation 2'), icons = map(['menu', 'zoom-in', 'star-empty'], function (i) { return $('<span class="icon icon-' + i + '" />'); }), iconspan = $('<span/>').append(icons), menu = $('<div class="math-ui-focus-menu" />').append(eraser, name, iconspan);
         if ($item.hasClass('focus'))
             return;
         $item.addClass('focus').append(menu).blur(function () {
@@ -25,6 +40,24 @@ var FlorianMath;
             contentElement = $('<content select=".math-ui-focus-menu" />');
             $(this.shadowRoot).append(contentElement);
         }
+        display_inline = $item.css('display') === 'inline';
+        if (display_inline)
+            $item.css('display', 'inline-block'); // correct width on Chrome
+        var item_padding = 2, focus_border_width = 2, menu_left = 8, itemWidth = getWidth(this);
+        if (itemWidth >= 0) {
+            log(itemWidth - 2 * item_padding - focus_border_width - menu_left);
+            eraser.width(itemWidth - 2 * item_padding - focus_border_width - menu_left);
+        }
+        if (display_inline)
+            $item.css('display', 'inline');
+        icons[0].on('mousedown', function (ev) {
+            var items = $('<div/>');
+            stopEvent(ev);
+            FlorianMath.each(['Get markup', 'Convert to code', 'Open with', 'Share', 'Search', 'Speak'], function (label) {
+                items.append($('<a href="#" />').append(label));
+            });
+            iconspan.append(items);
+        });
     }
     FlorianMath.initialized().then(function () {
         log('initialized');
