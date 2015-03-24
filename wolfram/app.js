ca20f0da3e9aedfb07ca447ddefcbf52b508b6a8
Jan Marthedal Rasmussen <jan@janmr.com>
2015-03-24 15:22:41 +0100

    Zoom, select, in-place commands

--- wolfram/app.js
+++ wolfram/app.js
@@ -2,7 +2,7 @@
 /// <reference path="jquery.d.ts" />
 var FlorianMath;
 (function (FlorianMath) {
-    var ACTIVE_CLASS = 'active', focusItem, hoverItem, menuItem, menuRemover;
+    var global = window, doc = document, ACTIVE_CLASS = 'active', focusItem, hoverItem, menuItem, menuRemover;
     function map(list, fn) {
         var result = [];
         FlorianMath.each(list, function (item) {
@@ -12,7 +12,7 @@ var FlorianMath;
     }
     var log = function (a1, a2) {
     };
-    if ('console' in window)
+    if ('console' in global)
         log = function (a1, a2) {
             if (a2 !== undefined)
                 console.log(a1, a2);
@@ -27,13 +27,72 @@ var FlorianMath;
         ev.stopPropagation();
         ev.preventDefault();
     }
+    function getName(mathItem) {
+        return 'Equation ' + (mathItem._private.id + 1);
+    }
+    // Zoom
+    function zoomAction(item) {
+        var popup = $('<div class="math-ui item-zoom" />'), contentRef, mathItemClone = item.cloneNode(true);
+        function handler(ev) {
+            if (ev.type === 'mousedown' && ev.which !== 1)
+                return;
+            ev.stopPropagation();
+            if (contentRef)
+                contentRef.remove();
+            popup.remove();
+            $(doc).off('mousedown keydown', handler);
+        }
+        HTMLMathItemElement.manualCreate(mathItemClone, true);
+        mathItemClone.clean();
+        popup.append(mathItemClone);
+        $(doc).on('mousedown keydown', handler);
+        $(item).append(popup);
+        if (item.shadowRoot) {
+            contentRef = $('<content select=".item-zoom" />');
+            $(item.shadowRoot).append(contentRef);
+        }
+        HTMLMathItemElement.manualAttach(mathItemClone, true);
+    }
+    var preventBlur = false;
     function showMenu(item) {
-        var contentElement, $item = $(item), display_inline, eraser = $('<div class="eraser"/>'), name = $('<span/>').append('Equation 2'), icons = map(['menu', 'zoom-in', 'star-empty'], function (i) { return $('<span class="icon icon-' + i + '" />'); }), iconspan = $('<span/>').append(icons), menu = $('<div class="math-ui-focus-menu" />').append(eraser, name, iconspan);
+        var contentElement, $item = $(item), display_inline, bodyHandler, eraser = $('<div class="math-ui eraser"/>'), icons = map(['menu', 'zoom-in', 'star-empty', 'question'], function (i) { return $('<span class="icon icon-' + i + '" />'); }), top = $('<div class="math-ui top" />').append($('<span class="math-ui eqn-name" />').append(getName(item)), icons), body = $('<div class="math-ui body" />'), menu = $('<div class="math-ui focus-menu" />').append(eraser, top, body);
+        function getMarkup() {
+            bodyHandler = function (ev) {
+                log('bodyHandler');
+                //ev.stopPropagation();
+            };
+            body.empty().append($('<select><option selected>MathML</option><option>TeX</option></select>'), $('<textarea rows="5" />').append(doc.createTextNode('<math>\n  <mi>x</mi>\n</math>')));
+        }
+        function showCommands() {
+            var items = $('<div class="math-ui list-group" />');
+            FlorianMath.each(['Get markup', 'Convert to code', 'Open with', 'Share', 'Search', 'Speak'], function (label) {
+                items.append($('<a href="#" />').append(label));
+            });
+            body.empty().append(items);
+            /*bodyHandler = (ev) => {
+                stopEvent(ev);
+                items.children().each(function (k) {
+                    if (ev.target === this) {
+                        if (k === 0) {
+                            getMarkup();
+                        }
+                    }
+                });
+            };*/
+        }
+        function doZoom() {
+            $item.blur();
+            zoomAction(item);
+        }
+        function toggleSelected() {
+            var on = item.selected = !item.selected;
+            icons[2].toggleClass('icon-star-empty', !on).toggleClass('icon-star-full', on);
+        }
         if ($item.hasClass(ACTIVE_CLASS))
             return;
         $item.addClass(ACTIVE_CLASS).append(menu);
         if (item.shadowRoot) {
-            contentElement = $('<content select=".math-ui-focus-menu" />');
+            contentElement = $('<content select=".focus-menu" />');
             $(item.shadowRoot).append(contentElement);
         }
         display_inline = $item.css('display') === 'inline';
@@ -41,18 +100,42 @@ var FlorianMath;
             $item.css('display', 'inline-block'); // correct width on Chrome
         var item_padding = 2, focus_border_width = 2, menu_left = 8, itemWidth = getWidth(item);
         if (itemWidth >= 0) {
-            //log(itemWidth - 2*item_padding - focus_border_width - menu_left);
             eraser.width(itemWidth - 2 * item_padding - focus_border_width - menu_left);
         }
         if (display_inline)
             $item.css('display', 'inline');
-        icons[0].on('mousedown', function (ev) {
-            var items = $('<div/>');
-            stopEvent(ev);
-            FlorianMath.each(['Get markup', 'Convert to code', 'Open with', 'Share', 'Search', 'Speak'], function (label) {
-                items.append($('<a href="#" />').append(label));
+        body.on('mousedown keydown', function (ev) {
+            if (ev.type === 'mousedown' && ev.which !== 1)
+                return;
+            //preventBlur = true;
+            if (bodyHandler)
+                bodyHandler(ev);
+        });
+        /*body.on('mouseup', () => {
+            preventBlur = false;
+            log('mouseup', preventBlur);
+        });*/
+        menu.on('mousedown keydown', function (ev) {
+            if (ev.type === 'mousedown' && ev.which !== 1)
+                return;
+            //log('menu handler');
+            top.children().each(function (k) {
+                if (ev.target === this) {
+                    stopEvent(ev);
+                    switch (k) {
+                        case 0:
+                        case 1:
+                            showCommands();
+                            break;
+                        case 2:
+                            doZoom();
+                            break;
+                        case 3:
+                            toggleSelected();
+                            break;
+                    }
+                }
             });
-            iconspan.append(items);
         });
         menuItem = item;
         menuRemover = function () {
@@ -73,12 +156,16 @@ var FlorianMath;
             showMenu(item);
     }
     function onFocus() {
+        //log('onFocus', this);
         focusItem = this;
         checkState();
     }
     function onBlur() {
-        focusItem = null;
-        checkState();
+        //log('onBlur', this);
+        if (!preventBlur) {
+            focusItem = null;
+            checkState();
+        }
     }
     function onMouseEnter() {
         hoverItem = this;
