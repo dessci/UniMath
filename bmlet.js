f63067970976fcf6479468465271ca9dc283c5b3
Jan Marthedal Rasmussen <jan.marthedal@gmail.com>
2015-03-26 09:38:37 +0100

    Update bmlet.js

--- bmlet.js
+++ bmlet.js
@@ -1,12 +1,12 @@
 (function () {
 
-var e, k;
+var s, e, a, k;
 
 document.querySelector('html').style.marginLeft = "300px";
 
-e = document.createElement('style');
-e.textContent="#math-ui { position: fixed; top: 0; bottom: 0; left: 0; width: 286px; background-color: #fff; border-right: 4px solid #888; padding: 5px; overflow-y: scroll; color: #000; text-align: left; z-index: 1000; } #math-ui p { font: 14px/16px sans serif; margin: 4px 0; }";
-document.querySelector('head').appendChild(e);
+s = document.createElement('style');
+s.textContent="#math-ui { position: fixed; top: 0; bottom: 0; left: 0; width: 286px; background-color: #fff; border-right: 4px solid #888; padding: 5px; overflow-y: scroll; color: #000; text-align: left; z-index: 1000; } #math-ui p { font: 14px/16px sans serif; margin: 4px 0; }";
+document.querySelector('head').appendChild(s);
 
 e = document.createElement('div');
 e.setAttribute('id', 'math-ui');
@@ -16,6 +16,17 @@ function para(txt) {
   p.appendChild(document.createTextNode(txt));
   return p;
 }
+
+a = document.createElement('a');
+a.href = "#";
+a.appendChild(document.createTextNode('[remove sidebar]'));
+a.addEventListener('click', function (e) {
+  e.preventDefault();
+  document.querySelector('html').style.marginLeft = '';
+  document.querySelector('head').removeChild(s);
+  document.body.removeChild(e);
+});
+e.appendChild(a);
 
 e.appendChild(para('This is the sidebar'));
 e.appendChild(para('Item properties can be put here'));
