1f8a6084b8cf5c9b760e20769e1577f826c025bf
Jan Marthedal Rasmussen <jan@janmr.com>
2015-03-27 14:50:10 +0100

    sidebar demo

---
+++ sidebar/bower_components/math-item/eqnstore-source.js
@@ -0,0 +1,24 @@
+/// <reference path="../dist/math-item.d.ts" />
+(function (global, doc) {
+    if (global.HTMLMathItemElement) {
+        var origRender = global.HTMLMathItemElement.prototype.render;
+        global.HTMLMathItemElement.prototype.render = function () {
+            var sources = this.getSources({ render: true, type: 'image/png' });
+            if (sources.length) {
+                var output = FlorianMath.mathItemInsertContent(this), img = doc.createElement('img'), styles = [];
+                img.src = sources[0].getAttribute('src');
+                if (sources[0].getAttribute('width'))
+                    styles.push('width:' + sources[0].getAttribute('width') + ';');
+                if (sources[0].getAttribute('valign'))
+                    styles.push('vertical-align:' + sources[0].getAttribute('valign') + ';');
+                if (styles.length)
+                    img.setAttribute('style', styles.join(' '));
+                output.element.appendChild(img);
+                output.done();
+            }
+            else
+                origRender.call(this);
+        };
+    }
+})(window, document);
+//# sourceMappingURL=eqnstore-source.js.map
